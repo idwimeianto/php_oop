18d3dfc518485ce68d6f66ea10dc670ecc7a6ca6
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2022-04-29 00:37:07 +0700

    remove unused lines

--- 11.pdo/03_pdo_fetchAll.php
+++ 11.pdo/03_pdo_fetchAll.php
@@ -10,20 +10,4 @@ $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
 echo "<pre>";
 echo print_r($arr);
 echo "</pre>"
-
-// OUTPUT
-/*
-  Array
-  (
-      [id] => 1
-      [nim] => 1400501
-      [nama] => Riana Putria
-      [tempat_lahir] => Padang
-      [tanggal_lahir] => 1996-11-23
-      [jenis_kelamin] => 
-      [fakultas] => FMIPA
-      [jurusan] => Kimia
-      [ipk] => 3.10
-  )
-*/
 ?>
